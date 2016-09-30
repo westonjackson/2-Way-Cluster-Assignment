158040a9d64d5baeef8e5ce86d35887b40ce4f5e
Weston Jackson <westonjackson2106@gmail.com>
2016-09-30 02:54:54 -0400

    added same covariance function

--- uijj_distribution.py
+++ uijj_distribution.py
@@ -3,11 +3,19 @@ import numpy as np
 import matplotlib.mlab as mlab
 import math
 
+def same_covariance_estimate(muj1, muj2, sigma1, sigma2, x):
+    diff = muj1 - muj2
+    x_diff = x - muj2
+    top = np.dot(np.dot(diff, np.linalg.inv(sigma1)), x_diff.T)
+    bottom = np.dot(np.dot(diff, np.linalg.inv(sigma1)), diff.T)
+    return top/float(bottom)
+
+
 if __name__ == '__main__':
     muj1 = np.array([[0,0]])
     muj2 = np.array([[1,0]])
-    sigma1 = np.array([[1,0],[0,1]])
-    sigma2 = np.array([[1,0],[0,1]])
+    sigma1 = np.array([[1.1,1.2],[1.2,2.3]])
+    sigma2 = np.array([[1.1,1.2],[1.2,2.3]])
     x = np.array([[0.5,0.5]])
 
     n = 100
@@ -30,5 +38,6 @@ if __name__ == '__main__':
         normal_results[0,i] = u
         normal_results[1,i] = result[0][0]
 
+    print same_covariance_estimate(muj1, muj2, sigma1, sigma2, x)
     plt.scatter(normal_results[0], normal_results[1])
     plt.show()
